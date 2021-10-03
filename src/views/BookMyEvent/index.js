1b25f24df509d15459c7b15f781aa9926e7365ff
alexisraimbault <27raimbault.alexis@gmail.com>
2021-10-03 22:31:10 +0200

    compute potential events from timespans and types

--- src/views/BookMyEvent/index.js
+++ src/views/BookMyEvent/index.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import * as _ from 'lodash';
 import axios from 'axios';
 import { serverURL } from '../../statics';
 import { useHistory } from 'react-router-dom';
+import * as moment from 'moment';
 
 import Input from '../../kit/Input';
 import ActionButton from '../../kit/ActionButton';
@@ -13,8 +14,22 @@ import './styles.scss';
 
 const BookMyEvent = ({ props }) => {
     const history = useHistory();
-
+    const availableSpans = useRef([]);
+    const [types, setTypes] = useState([]);
     const sessionId = 4;
+
+    const selectType = typeIdx => {
+        const newTypes =_.cloneDeep(types);
+
+        _.each(newTypes, newType => newType.selected=false);
+        newTypes[typeIdx].selected =true;
+
+        setTypes(newTypes);
+    };
+
+    const selectPotential = potentialIdx => {
+        // TODO
+    };
 
     useEffect(() => {
         axios.get(`${serverURL}/session/booking`,{
@@ -22,16 +37,71 @@ const BookMyEvent = ({ props }) => {
         })
         .then(res => {
             console.log('ALEXIS res', res);
+            availableSpans.current = _.get(res, 'data.available', []);
+            setTypes(_.map(_.get(res, 'data.types', []), type => _.assign(type, {selected: false})));
+
         }).catch(err => {
             console.log('ALEXIS err', err);
         });
     }, []);
 
+    const generatePotentials = (spans, type) => {
+        console.log('ALEXIS generatePotentials',{spans,type});
+        const durationMinutes = _.parseInt(_.get(type, 'duration.minutes', 0)) + (60 *  _.parseInt(_.get(type, 'duration.hours', 0)));
+
+        const potentialsRes = [];
+        // V1 : only stack from beginning
+        _.each(spans, span => {
+            let stackCpt = 1;
+            const nbMinutesInSpan = moment(span.to).diff(span.from, 'minutes');
+
+            console.log('ALEXIS nbMinutesInSpan', nbMinutesInSpan);
+            console.log('ALEXIS durationMinutes', durationMinutes);
+            while(durationMinutes * stackCpt <= nbMinutesInSpan) {
+                potentialsRes.push({
+                    from: moment(span.from).add(durationMinutes * (stackCpt - 1), 'minutes'),
+                    to: moment(span.from).add(durationMinutes * stackCpt, 'minutes')
+                });
+                
+                stackCpt++;
+            }
+        });
+
+        return potentialsRes;
+    };
+
+    const selectedTypeIdx = _.findIndex(types, {selected: true});
+    console.log('ALEXIS selectedTypeIdx', selectedTypeIdx);
+    const potentials = selectedTypeIdx === -1 ? [] : generatePotentials(availableSpans.current, types[selectedTypeIdx])
+
+    console.log('ALEXIS potentials', potentials);
     return (
         <div>
-            hello from booking page
+            { _.map(types, (type, typeIdx) => {
+
+                return (
+                    <div 
+                            className={`type ${type.selected ? 'type-selected': ''}`}
+                            onClick={_.partial(selectType, typeIdx)}
+                        >
+                            {type.name}
+                        </div>
+                )
+            })}
+
+            { _.map(potentials, (potential, potentialIdx) => {
+
+            return (
+                <div 
+                    className={`potential`}
+                    onClick={_.partial(selectPotential, potentialIdx)}
+                >
+                    {`${moment(potential.from).format("dddd, MMMM Do YYYY, h:mm a")} - ${moment(potential.to).format("dddd, MMMM Do YYYY, h:mm a")}`}
+                </div>
+            )
+            })}
         </div>
-    )
+    );
 }
 
 export default BookMyEvent;
